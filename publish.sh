e26398e0b285fe2d3a5fccfddbea50e31938e638
Robin Houston <robin.houston@gmail.com>
2012-07-24 11:18:07 +0100

    New tabbed version

--- publish.sh
+++ publish.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
-aws put -v "Content-type: text/html" "x-amz-acl:public-read" \
-    s3.boskent.com/prisoners-dilemma/ipd.html ipd.html
+set -e
+
+unset CDPATH
+cd "$(dirname "$0")"
+
+for f in *.html
+do
+    aws put -v "Content-type: text/html" "x-amz-acl:public-read" \
+        s3.boskent.com/prisoners-dilemma/"$f" "$f"
+done
+
+for f in *.js
+do
+    aws put -v "Content-type: application/javascript" "x-amz-acl:public-read" \
+        s3.boskent.com/prisoners-dilemma/"$f" "$f"
+done
+
+for f in *.css
+do
+    aws put -v "Content-type: text/css" "x-amz-acl:public-read" \
+        s3.boskent.com/prisoners-dilemma/"$f" "$f"
+done
